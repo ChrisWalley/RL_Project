b4bf274b13ad7177186932ea39dcd2dbbc805fae
Anton Stott <antonstott@gmail.com>
2022-10-30 13:51:54 +0200

    MCTS stuff

--- environments/QuestEnvironment.py
+++ environments/QuestEnvironment.py
@@ -5,6 +5,8 @@ from nle import nethack
 from minihack import RewardManager
 from gym.envs.classic_control import rendering
 
+SEED = 42
+
 # wrapper for rendering the env as an image
 class RenderingWrapper(gym.Wrapper):
 
@@ -21,11 +23,33 @@ class RenderingWrapper(gym.Wrapper):
             vsync=False, 
             resizable=True
         )
+        self.action_history = []
+
+    def clone(self):
+
+        env_clone = QuestEnvironment().create(
+            reward_lose = -10,
+            reward_win = 10,
+            penalty_step = -0.002,
+            penalty_time = -0.001
+        )
+
+        env_clone.reset()
+
+        # take all the actions the other environment took
+        for action in self.action_history:
+            _ = env_clone.step(action)
+
+        return env_clone
+            
+        
 
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
+        self.action_history.append(action)
         self.pixels = obs['pixel']
-        return obs['glyphs_crop'], reward, done, info
+        #return obs['glyphs_crop'], reward, done, info
+        return obs, reward, done, info
 
     def render(self, mode="human", **kwargs):
         if mode == 'human':
@@ -39,7 +63,8 @@ class RenderingWrapper(gym.Wrapper):
         self.pixels = obs['pixel']
 
         # TODO: make sure this is ok
-        return obs['glyphs_crop']
+        #return obs['glyphs_crop']
+        return obs
 
     def close(self):
         if self.viewer is not None:
@@ -51,7 +76,11 @@ class QuestEnvironment:
 
     def _get_actions(self):
 
-        return tuple(nethack.CompassDirection) + (
+        return (
+            nethack.CompassCardinalDirection.N,
+            nethack.CompassCardinalDirection.E,
+            nethack.CompassCardinalDirection.S,
+            nethack.CompassCardinalDirection.W,
             nethack.Command.SEARCH,
             nethack.Command.KICK,
             nethack.Command.OPEN,
@@ -71,12 +100,27 @@ class QuestEnvironment:
             nethack.Command.INVOKE,
             nethack.Command.RUSH,
             nethack.Command.WEAR,
-            nethack.Command.ENHANCE
+            nethack.Command.ENHANCE,
+            nethack.Command.MOVE,
+            nethack.Command.MOVEFAR,
+            nethack.Command.FIGHT
         )
 
     # create the environment
     # https://minihack.readthedocs.io/en/latest/envs/skills/quest.html
-    def create(self):
+    def create(
+            self,
+            reward_lose = -10,
+            reward_win = 10,
+            penalty_step = -0.002,
+            penalty_time = 0.002,
+            max_episode_steps = 5000
+        ):
+
+        import numpy as np
+
+        self.visited_states_map = np.zeros((21,79)) # a map of counts for each state
+        
 
         # setup the reward manager
         # https://minihack.readthedocs.io/en/latest/getting-started/reward.html?highlight=RewardManager#reward-manager
@@ -85,19 +129,30 @@ class QuestEnvironment:
         reward_manager.add_kill_event("goblin", reward=1)
         reward_manager.add_kill_event("jackal", reward=1)
         reward_manager.add_kill_event("giant rat", reward=1)
+        
+        def exploration_reward(env, obs, action, nobs):
+            reward = 0
+            return reward
+
+        reward_manager.add_custom_reward_fn(exploration_reward)
 
         # make the environment
         env = gym.make(
             "MiniHack-Quest-Hard-v0",
             actions = self._get_actions(),
             reward_manager = reward_manager,
-            observation_keys = ("glyphs", "pixel", "glyphs_crop"),
-            reward_lose = -10,
-            reward_win = 10,
-            penalty_step = -0.002,
-            penalty_time = 0.002,
+            observation_keys = ("glyphs", "pixel", "glyphs_crop", "blstats"),
+            reward_lose = reward_lose,
+            reward_win = reward_win,
+            penalty_step = penalty_step,
+            penalty_time = penalty_time,
+            max_episode_steps = max_episode_steps
         )
 
         env = RenderingWrapper(env)
+
+        env.seed(SEED)
+
+        #print(f"Number of actions: {env.action_space.n}")
 
         return env
